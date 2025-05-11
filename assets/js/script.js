44cca3e5fc2a83167c2a76080ae73f891f3c8844
lucyinthesky2111 <lucy.kidner@outlook.com>
2025-05-11 23:09:08 +0100

    Add starter code for introduction modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
+
+const openIntroductionModal = document.getElementById("open-introduction-modal")
+const introductionModal = document.getElementById("introduction-modal")
+const close = document.getElementById("close")
+const play = document.getElementById("play")
+const howToPlay = document.getElementById("how-to-play")
+
+
+openIntroductionModal.addEventListener('click', function () {
+    introductionModal.style.display = "block";
+
+})
+
+close.addEventListener('click', function () {
+    introductionModal.style.display = "none";
+
+})
+
+// When the user clicks anywhere outside of the modal, close the instructions
+window.addEventListener("click", function(event) {
+  if (event.target === introductionModal) {
+    introductionModal.style.display = "none";
+  }
+});
+
+
+
 function startGame() {
 
 }
