55e9a6930872f5cbe3a477d6c422bebfad549dd2
lucyinthesky2111 <lucy.kidner@outlook.com>
2025-05-18 22:37:22 +0100

    Change modal default display to none on all screen sizes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded',  function() {
   // Gets the introduction modal
   let introductionModal = document.getElementById("introduction-modal");
 
-  // Only show intro modal if user is on index.html/root
+  // Only show intro modal if user is on index.html/root - code from ChatGPT (see Readme credits)
   const isIndexPage =
   /* Modal is hidden by default in css. This code checks the user is on index.html or root and if this is trues and (&&) the modal is called,
   modal will be displayed */
@@ -11,8 +11,9 @@ document.addEventListener('DOMContentLoaded',  function() {
     introductionModal.style.display = "block";
   }
 
-  // CODE FROM W3SCHOOL
-  // Closes the introduction modal when the user clicks on the 'X' icon
+  // 
+  /* Closes the introduction modal when the user clicks on the 'X' icon - this line of code is from 
+  https://github.com/Diane-4P/harry-potter-quiz/blob/main/assets/js/script.js*/
   const close = document.getElementById("close"); {
     close.addEventListener('click', function () {
       introductionModal.style.display = "none";
