818f58a4917abe51cd8bb355246a15505e525ecd
lucyinthesky2111 <lucy.kidner@outlook.com>
2025-05-19 23:10:02 +0100

    Add questions

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -20,7 +20,40 @@
     })
   }
 
+// Easy questions as objects within an array. Possible answers are an array within the object 
+   const easyQuestions = [
 
+    {
+      questionNo: '1.',
+      question: "What is the name of the coffee shop where the friends go to hang out?",
+      possibleAnswers: ['Central Park', 'Coffee 1', 'Costa', 'Central Perk'],
+      correctAnswer: 'Central Perk',
+    },
+
+    {
+      questionNo: '2.',
+      question: "Which two characters are brother and sister?",
+      possibleAnswers: ['Monica and Ross', 'Ross and Rachel', 'Phoebe and Joey', 'Monica and Chandler'],
+      correctAnswer: 'Monica and Ross',
+
+    },
+
+    {
+      questionNo: '3.',
+      question: "How many sisters does Joey have?",
+      possibleAnswers: ['2', '6', '7', 'None'],
+      correctAnswer: '7',
+
+    },
+
+     {
+      questionNo: '4.',
+      question: "Which friend had jobs at Central Perk, Bloomingdales and Ralph Lauren?",
+      possibleAnswers: ['Monica', 'Chandler', 'Phoebe', 'Rachel'],
+      correctAnswer: 'Rachel',
+
+    },
+  ]
 
 
   }); // Closing tag for load DOM content event listener
