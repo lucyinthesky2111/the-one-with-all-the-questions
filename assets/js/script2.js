ad52f640787b0f9170d75d1cccda76ebf9693e97
lucyinthesky2111 <lucy.kidner@outlook.com>
2025-05-20 22:43:58 +0100

    Add JS to make correct divs show and hide as necessary upon selection of difficulty

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -63,7 +63,8 @@
   let score = 0;
 
   // Code help from 'Love Maths' walkthrough project 
-  function startGame() {
+  function startGame() { 
+    document.getElementById("container").style.display = "none";
     let buttons = document.querySelectorAll("button[data-type]");
 
     for (let button of buttons) {
@@ -72,8 +73,10 @@
         if (type === "easy") {
           alert("Loading 'easy' questions! 'Good luck, good luck, we all wish you good luck!'");
           displayEasyQuestions();
-        } else {
-          alert(`"${type}" difficulty not available yet.`);
+        } else if (type === "medium") {
+          alert("Loading 'medium' questions! 'Good luck, good luck, we all wish you good luck!'");
+        } else if (type === "hard") {
+          alert("Loading 'hard' questions! 'Good luck, good luck, we all wish you good luck!'");
         }
       });
     }
@@ -83,16 +86,58 @@
     currentDifficulty =(easyQuestions);
     currentQuestionIndex = 0;
     score = 0;
-
-
+    
+    document.getElementById("container").style.display = "block";
+    document.getElementById("question-area").style.display = "none";
     document.getElementById("select-difficulty").style.display = "none"
     document.getElementById("easy").style.display = "none";
     document.getElementById("medium").style.display = "none";
     document.getElementById("hard").style.display = "none";
 
-    showEasyQuestion();
+    displayEasyQuestions();
   }
 
+  // Variables
+  const quizProgress = document.getElementById("quiz-progress");
+  const questionText = document.getElementById("question-text");
+  const possibleAnswers = document.getElementById("possible-answers");
+
+
+  function handleQuestion(index) {
+    // Quiz progress bar which appears above questions and possible answers
+    /* for each easy question, within the quiz progress div, add a span which is styled as a grey progress bar using a CSS class, each bar will later
+    turn yellow one by one, as the user clears each question. */
+    easyQuestions.forEach((question) => {
+      // this line creates the span
+      const span = document.createElement("span");
+      // this line adds the span to the correct place i.e. the div with the id of quiz-progress 
+      quizProgress.appendChild(span);
+    });
+    /* Loops through each span tag (which corresponds with each question) and adds my 'seen' class to each span incremently to change the colour 
+    of the bar to yellow */ 
+    // Sets a variable called spans and selects all the spans in the div with id of quiz-progress 
+    let spans = document.querySelectorAll("#quiz-progress span");
+    // Starts the count at zero and continues until it reaches the end of the questions. Increases the count by one each time 
+    for (let i = 0; i <= index; i++) {
+      // Everytime the count is increased, the 'seen' class is added to the corresponding span
+      spans[i].classList.add("seen");
+    };
+
+    // Display question number and question
+    questionText.innerHTML =
+    `<p>${easyQuestions[index].questionNo}</p>
+    <p>${easyQuestions[index].question}</p>
+    `
+    
+    // Display answers
+    // Adds a button containing each possible answer (which is styled with CSS) 
+    easyQuestions[index].possibleAnswers.forEach(answer => {
+      possibleAnswers.innerHTML += `<button>${answer}</button>`;
+    });
+  
+    } //Closing tag for handleQuestion function
+
+  handleQuestion(currentQuestionIndex);
 
 
   startGame();
